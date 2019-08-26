eec702564410548c32bdc10753d58bab7ef06704
wooyuncr <woo.yun@chromeriver.com>
2019-08-26 00:00:54 -0700

    Add changes to MainApplication.java too

--- android/app/src/main/java/com/pys/dazzleddate/MainApplication.java
+++ android/app/src/main/java/com/pys/dazzleddate/MainApplication.java
@@ -3,6 +3,10 @@ package com.pys.dazzleddate;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.evollu.react.fcm.FIRMessagingPackage;
+import io.invertase.firebase.RNFirebasePackage;
+import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
+import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.brentvatne.react.ReactVideoPackage;
 import com.zyu.ReactNativeWheelPickerPackage;
@@ -14,7 +18,6 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-
 import java.util.Arrays;
 import java.util.List;
 
@@ -30,6 +33,10 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new FIRMessagingPackage(),
+            new RNFirebasePackage(),
+            new RNFirebaseNotificationsPackage(),
+            new RNFirebaseMessagingPackage(),
             new RNDeviceInfo(),
             new ReactVideoPackage(),
             new ReactNativeWheelPickerPackage(),
